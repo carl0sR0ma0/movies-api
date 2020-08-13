38750d17d6092a41f1e54ca24d044639d2ff14da
carl0sR0ma0 <cvpromao@gmail.com>
2020-08-13 20:26:45 -0300

    Criando o schema de movies

---
+++ src/app/models/movies.models.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose')
+
+const MovieSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  genre: {
+    type: String,
+    required: false,
+    trim: true
+  },
+  age: {
+    type: Number,
+    required: false
+  },
+  older18: {
+    type: Boolean,
+    required: true
+  }
+},
+  {
+    timestamps: true,
+    versionKey: false
+  }
+)
+module.exports = model('fileschema', FileSchema)
