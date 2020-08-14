b3212be06ed634b4263bef55ffbba54a7c3a5288
carl0sR0ma0 <cvpromao@gmail.com>
2020-08-13 22:20:11 -0300

    Criando as rotas de buscar Flmes

--- src/app/routes/filmes.routes.js
+++ src/app/routes/filmes.routes.js
@@ -3,5 +3,7 @@ const route = express.Router()
 const Filme = require('./../controllers/filmes.controller')
 
 route.post('/criar', Filme.criarFilme)
+route.get('/visualizarTodos', Filme.visualizarFilmes)
+route.get('/visualizarUm/:nome', Filme.visualizarUmFilme)
 
 module.exports = route
