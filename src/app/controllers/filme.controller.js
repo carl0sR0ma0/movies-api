1b59f9f2792d29712061b3ab7e7d0635cd322ce0
carl0sR0ma0 <cvpromao@gmail.com>
2020-10-16 09:35:27 -0300

    Implementando o método delete e disponibilizando na rota para poder apagar um filme.

--- src/app/controllers/filme.controller.js
+++ src/app/controllers/filme.controller.js
@@ -135,55 +135,30 @@ class Filme {
     })
   }
 
-  // atualizarUmFilme(req, res) {
-  //   //-- Guarda na constante nomeDoFilmeParaSerAtualizado o parâmetro nome que vem da URL do endpoint.
-  //   const nomeDoFilmeParaSerAtualizado = req.params.nome
-  //   //-- Guarda na constante novoNomeDoFilme o valor da chave nome que vem do corpo da requisição
-  //   const novoNomeDoFilme = req.body.nome
-    
-  //   /**
-  //    * Chamando o método de update do banco de dados e passando o nome que deve ser atualizado, e
-  //    * e passando para $set o novo nome através do corpo da requisição
-  //   **/   
-  //   filme.updateOne({nome: nomeDoFilmeParaSerAtualizado}, { $set: req.body }, (err, data) => {
-  //     if (err) {
-  //       //-- Aqui só ocorre quando tem algum erro ao tentar atualizar
-  //       res.status(500).send({message: "Houve um erro ao processar a sua atualização", error: err})
-  //     } else {
-  //       //-- Verifica se n(numero de registros modificados) é maior que 0, ou seja que o registro foi modificado
-  //       if (data.n > 0) {
-  //         //-- Aí ele faz uma busca, procurando no banco o novo nome do filme passado
-  //         filme.findOne({nome: novoNomeDoFilme}, (err, result) => {
-  //           if (err) {
-  //             res.status(500).send({message: "Houve um erro ao processar a sua busca no filme atualizado", error: err})
-  //           } else {
-  //             /*
-  //              * Se não tiver erro ele retorna uma mensagem dizendo que o filme que foi atualizado
-  //              * teve o nome atualizado para o novo nome do filme
-  //              */
-  //             res.status(200).send({message: `Filme ${nomeDoFilmeParaSerAtualizado} teve seu nome 
-  //             atualizado para ${novoNomeDoFilme}`, filme: result })
-  //           }
-  //         })
-  //       }
-  //     }
-  //   })
-  // }
-  
-  
-  // apagarUmFilme(req, res) {
-  //   //-- Guardando numa variável o valor do parâmetro nome do endpoint.
-  //   const nomeDoFilmeParaSerApagado = req.params.nome
+  delete(req, res) {
+    const { movieId } = req.params
 
-  //   //-- Chamando o método deleteOne que vai apagar o registro do banco de dados, nele passa o nome do filme que vai ser apagado.
-  //   filme.deleteOne({nome: nomeDoFilmeParaSerApagado}, (err) => {
-  //     if (err) {
-  //       res.status(500).send({message: "Houve um erro ao apagar um", error: err })
-  //     } else {
-  //       res.status(200).send({message: `O filme ${nomeDoFilmeParaSerApagado} foi apagado com sucesso!`})
-  //     }
-  //   })
-  // }
+    diretor.findOne({ filmes: movieId }, (err, diretor) => {
+      if (err) {
+        res.status(500).send({message: "Houve um erro ao processo ao processar sua requisição", error: err })
+      } else {
+        diretor.filmes.pull(movieId)
+        diretor.save({}, (err) => {
+          if (err) {
+            res.status(500).send({message: "Houve um erro ao processo ao processar sua requisição", error: err })
+          } else {
+            filme.deleteOne({_id: movieId}, (err, result) => {
+              if (err) {
+                res.status(500).send({message: "Houve um erro ao processo ao processar sua requisição", error: err })
+              } else {
+                res.status(200).send({message: "Filme foi apagado com sucesso", data: result })
+              }
+            })
+          }
+        })
+      }
+    })
+  }
 }
 
 module.exports = new Filme()
--- src/app/routes/filme.routes.js
+++ src/app/routes/filme.routes.js
@@ -7,5 +7,6 @@ route.get('/listarTodos', Filme.buscarTodosOsFilmes)
 route.get('/listarUm/:nomeFilme', Filme.buscarUmFilmePeloNome)
 route.get('/validarNomeFilme', Filme.validarNomeFilme)
 route.put('/atualizar/:movieId', Filme.update)
+route.delete('/apagar/:movieId', Filme.delete)
 
 module.exports = route
